fda3fc211fed6f2a26ed96e1627ff6da8f52d6e4
Asante Yeboah Israel <114362590+Willow7737@users.noreply.github.com>
2025-08-24 08:25:29 +0000

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -11,3 +11,11 @@ engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
+
+# Database dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
